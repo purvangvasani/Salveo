3ae1cc350f8b8372f6ac7a25696dfe0d30a1a216
PURVANG VASANI <purvangvasani4@gmail.com>
2024-06-01 19:23:38 +0530

    update home page name to user type names and separted out search page to make it component

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,25 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import {
   CButton,
   CFormInput,
   CInputGroup,
   CRow, CCol,
   CForm,
-  CFormFeedback, CTable,
-  CTableBody, CTableDataCell,
-  CTableHead, CTableHeaderCell, CTableRow
+  CFormFeedback
 } from '@coreui/react';
+import Typewriter from "typewriter-effect";
 import CIcon from '@coreui/icons-react';
-import { cilSearch, cilUserPlus } from '@coreui/icons';
-import { Link, useNavigate } from 'react-router-dom';
+import { cilSearch } from '@coreui/icons';
+import { useNavigate } from 'react-router-dom';
 
 
 const Home = () => {
 
   const [validated, setValidated] = useState(false)
-  const [showResult, setShowResult] = useState(false)
-  const [resultList, setResultList] = useState([])
   const [searchedItem, setSearchedItem] = useState('')
+
+  const navigate = useNavigate();
 
   const handleSearchedChange = (event) => {
     setSearchedItem(event.target.value);
@@ -31,34 +30,9 @@ const Home = () => {
     if (form.checkValidity() === false) {
       event.stopPropagation()
     } else {
-      setShowResult(true)
-      getData()
-      // navigate('/search')
+      navigate('/search', { state: searchedItem })
     }
     setValidated(true)
-  }
-
-  const navigate = useNavigate();
-
-  const goTo = () => {
-    navigate('/users/add');
-  }
-
-  const searchRecords = (records, query) => {
-    const queryStr = query.toString().toLowerCase();
-
-    return records.filter(record => {
-      return Object.values(record).some(value =>
-        value.toString().toLowerCase().includes(queryStr)
-      );
-    });
-  }
-
-  const getData = async () => {
-    let staticData = await import('../../../data/backend-data.json').then((res) => res.default);
-    staticData = searchRecords(staticData.users, searchedItem);
-    console.log(staticData)
-    setResultList(staticData)
   }
 
   return (
@@ -66,76 +40,44 @@ const Home = () => {
       <CRow>
         <CCol xs={12} className="text-center">
           <span className="display-3">Welcome to <span style={{ color: '#6b7785' }}>Salveo</span>, </span><br />
-          <span className="display-4"><em>Purvang Vasani!</em></span>
+          <span className="display-4">
+            <em>
+              <Typewriter
+                options={{
+                  strings: ['Hospital Account!', 'Chemist Account!', 'User Account!'],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </em>
+          </span>
         </CCol>
-        {!showResult &&
-          <CRow>
-            <CCol xs={12} className='gy-4'>
-              <CButton
-                className="float-end"
-                color="secondary"
-                type="button"
-                id="search-record-btn"
-                onClick={goTo}>
-                <CIcon icon={cilUserPlus} className="me-2" />
-                Add Record
-              </CButton>
-            </CCol>
-            <CCol xs={12} className='gy-4'>
-              <CForm
-                className="row g-3 needs-validation"
-                noValidate
-                validated={validated}
-                onSubmit={handleSubmit}
-              >
-                <CCol md={12} className="position-relative">
-                  <CInputGroup className="mb-3">
-                    <CFormInput type="text" id="search-record-vlnd" onChange={handleSearchedChange}
-                      placeholder="Search Patient's Name, Aadhaar Number, SSN..." required />
-                    <CFormFeedback tooltip invalid>
-                      Please provide a Patient's Name, Aadhaar Number, SSN ...
-                    </CFormFeedback>
-                    <CButton
-                      color="secondary"
-                      type="submit"
-                      id="search-record-btn">
-                      <CIcon icon={cilSearch} />
-                    </CButton>
-                  </CInputGroup>
-                </CCol>
-              </CForm>
-            </CCol>
-          </CRow>
-        }
-        {showResult && <CCol xs={12} className='gy-4'>
-          <CTable hover>
-            <CTableHead>
-              <CTableRow>
-                <CTableHeaderCell scope="col">#</CTableHeaderCell>
-                <CTableHeaderCell scope="col">Name</CTableHeaderCell>
-                <CTableHeaderCell scope="col">Aadhaar Number</CTableHeaderCell>
-                <CTableHeaderCell scope="col">Email</CTableHeaderCell>
-                <CTableHeaderCell scope="col">City</CTableHeaderCell>
-              </CTableRow>
-            </CTableHead>
-            <CTableBody>
-              {resultList.length > 0 && resultList.map((data, index) =>
-                <CTableRow key={index}>
-                  <CTableHeaderCell scope="row">{index + 1}</CTableHeaderCell>
-                  <CTableDataCell>{data.firstname + " " + data.lastname}</CTableDataCell>
-                  <CTableDataCell>{data.aadhaar}</CTableDataCell>
-                  <CTableDataCell>{data.email}</CTableDataCell>
-                  <CTableDataCell>{data.city}</CTableDataCell>
-                </CTableRow>)
-              }
-              {resultList.length === 0 && 
-              <CTableRow>
-                <CTableDataCell className='text-center' colSpan={5}>Data not found!</CTableDataCell>
-              </CTableRow>
-              }
-            </CTableBody>
-          </CTable>
-        </CCol>}
+        <CRow>
+          <CCol xs={12} className='gy-4'>
+            <CForm
+              className="row g-3 needs-validation"
+              noValidate
+              validated={validated}
+              onSubmit={handleSubmit}
+            >
+              <CCol md={12} className="position-relative">
+                <CInputGroup className="mb-3">
+                  <CFormInput type="text" id="search-record-vlnd" onChange={handleSearchedChange}
+                    placeholder="Search Patient's Name, Aadhaar Number, SSN..." required />
+                  <CFormFeedback tooltip invalid>
+                    Please provide a Patient's Name, Aadhaar Number, SSN ...
+                  </CFormFeedback>
+                  <CButton
+                    color="secondary"
+                    type="submit"
+                    id="search-record-btn">
+                    <CIcon icon={cilSearch} />
+                  </CButton>
+                </CInputGroup>
+              </CCol>
+            </CForm>
+          </CCol>
+        </CRow>
       </CRow>
     </>
   )
--- src/views/search/Search.js
+++ src/views/search/Search.js
@@ -1,10 +1,113 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+
+import CIcon from '@coreui/icons-react';
+import { cilPencil, cilSettings, cilTrash, cilUserPlus } from '@coreui/icons';
+import {
+  CButton,
+  CCol, CDropdown, CDropdownHeader,
+  CDropdownItem, CDropdownMenu, CDropdownToggle,
+  CRow, CTable, CTableBody, CTableDataCell,
+  CTableHead, CTableHeaderCell, CTableRow
+} from '@coreui/react';
 
 const Search = () => {
+  const [resultList, setResultList] = useState([])
+
+  const searchedData = useLocation().state;
+
+  const searchRecords = (records, query) => {
+    const queryStr = query.toString().toLowerCase();
+
+    const data = records.filter(record => {
+      return Object.values(record).some(value =>
+        value.toString().toLowerCase().includes(queryStr)
+      );
+    });
+    setResultList(data);
+  }
+
+  const navigate = useNavigate();
+
+  const goTo = () => {
+    navigate('/users/add');
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const staticData = await import('../../../data/backend-data.json').then((res) => res.default);
+      searchRecords(staticData.users, searchedData);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div>
-        Search Page Here!
-    </div>
+    <>
+      <CRow>
+        <CCol xs={8} className='gy-4'>
+          Showing results for <b style={{ color: '#6b7785' }}><em>{searchedData}</em></b> !
+        </CCol>
+        <CCol xs={4} className='gy-4'>
+          <CButton
+            className="float-end"
+            color="secondary"
+            type="button"
+            id="search-record-btn"
+            onClick={goTo}>
+            <CIcon icon={cilUserPlus} className="me-2" />
+            Add Record
+          </CButton>
+        </CCol>
+        <CCol xs={12} className='gy-4'>
+          <CTable hover>
+            <CTableHead>
+              <CTableRow>
+                <CTableHeaderCell scope="col">#</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Name</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Aadhaar Number</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Email</CTableHeaderCell>
+                <CTableHeaderCell scope="col">City</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Actions</CTableHeaderCell>
+              </CTableRow>
+            </CTableHead>
+            <CTableBody>
+              {resultList.length > 0 && resultList.map((data, index) =>
+                <CTableRow key={index}>
+                  <CTableHeaderCell scope="row">{index + 1}</CTableHeaderCell>
+                  <CTableDataCell>{data.firstname + " " + data.lastname}</CTableDataCell>
+                  <CTableDataCell>{data.aadhaar}</CTableDataCell>
+                  <CTableDataCell>{data.email}</CTableDataCell>
+                  <CTableDataCell>{data.city}</CTableDataCell>
+                  <CTableDataCell>
+                    <CDropdown>
+                      <CDropdownToggle placement="bottom-end" className="py-0 pe-0" caret={false}>
+                        <CIcon icon={cilSettings} className="me-2" />
+                      </CDropdownToggle>
+                      <CDropdownMenu className="pt-0" placement="bottom-end">
+                        <CDropdownHeader className="bg-body-secondary fw-semibold mb-2">Actions</CDropdownHeader>
+                        <CDropdownItem href="/profile">
+                          <CIcon icon={cilPencil} className="me-2" />
+                          Edit
+                        </CDropdownItem>
+                        <CDropdownItem href="/settings">
+                          <CIcon icon={cilTrash} className="me-2" />
+                          Delete
+                        </CDropdownItem>
+                      </CDropdownMenu>
+                    </CDropdown>
+                  </CTableDataCell>
+                </CTableRow>)
+              }
+              {resultList.length === 0 &&
+                <CTableRow>
+                  <CTableDataCell className='text-center' colSpan={6}>Data not found!</CTableDataCell>
+                </CTableRow>
+              }
+            </CTableBody>
+          </CTable>
+        </CCol>
+      </CRow>
+    </>
   )
 }
 
