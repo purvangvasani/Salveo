bf52c7a9ebbbfa476a890f131abe57b372558dd6
PURVANG VASANI <purvangvasani4@gmail.com>
2024-06-02 09:42:27 +0530

    added dummy timeline feature

---
+++ src/views/search/view/View.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Timeline from '../../timeline/Timeline'
+
+const View = () => {
+    return (
+        <>
+            View Page Here!
+            <br />
+            <br />
+            <br />
+            <Timeline />
+        </>
+    )
+}
+
+export default View
